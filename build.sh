566247297e741e2c86918e2ec480b6b2e6b15186
ShawnAjamala <shawnajamala1@gmail.com>
2026-07-01 02:16:10 +0300

    create new superuser using build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+
+python manage.py migrate
+
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='Shawn_admin').exists():
-    User.objects.create_superuser('Shawn_admin', 'shawnajamala1@gmail.com', '123456')
+    User.objects.create_superuser('Shawn_admin', 'shawnajamala1@gmail.com', 'Shawn@1234')
 "
+
+python manage.py collectstatic --no-input
